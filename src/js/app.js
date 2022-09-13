36bcbc394a4e00aee78c3be9964a2d3a3ea811ff
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-09-12 22:08:20 -0600

    Javascript en la navegacion de moviles

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    eventListener();
+});
+
+function eventListener() {
+    const mobileMenu = document.querySelector('.movil-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+
+    
+}
+
+function navegacionResponsive(){
+
+    const navegacion = document .querySelector('.navegacion');
+
+    if(navegacion.classList.contains('mostrar')){
+        navegacion.classList.remove('mostrar');
+    }else{
+        navegacion.classList.add('mostrar');
+    }
+
+}
