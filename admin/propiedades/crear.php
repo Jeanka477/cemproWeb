23611c397e966b96bd8606e52796c39c6cc3faf8
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-10-02 11:05:43 -0600

    gregar imagenes a la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,19 +7,40 @@ $db =conectarDB();
 
 $errores = [];
 
+$titulo = '';
+$precio = '';
+$descripcion = '';
+$luz = '';
+$agua = '';
+$vista = '';
+$vendedorId = '';
+
 if ($_SERVER['REQUEST_METHOD'] ==='POST'){
-  //  echo "<pre>";
+
+
+  // echo "<pre>";
    // var_dump($_POST);
+    //echo "</pre>";
+
+   // echo "<pre>";
+   //var_dump($_FILES);
    // echo "</pre>";
 
-    $titulo = $_POST['titulo'];
-    $precio = $_POST['precio'];
-    $descripcion = $_POST['descripcion'];
-    $luz = $_POST['luz'];
-    $agua = $_POST['agua'];
-    $vista = $_POST['vista'];
-    $vendedorId = $_POST['vendedor'];
+   
 
+    $titulo = mysqli_real_escape_string( $db, $_POST['titulo']);
+    $precio =  mysqli_real_escape_string( $db, $_POST['precio']);
+    $descripcion  =  mysqli_real_escape_string( $db, $_POST['descripcion']);
+    $luz =  mysqli_real_escape_string( $db, $_POST['luz']);
+    $agua =  mysqli_real_escape_string( $db, $_POST['agua']);
+    $vista =  mysqli_real_escape_string( $db, $_POST['vista']);
+    $vendedorId =  mysqli_real_escape_string( $db, $_POST['vendedor']);
+
+
+    // asignar files hacia una variable 
+
+    $imagen = $_FILES['imagen'];
+   
 
     // validador de campos 
     if(!$titulo){
@@ -46,13 +67,44 @@ if ($_SERVER['REQUEST_METHOD'] ==='POST'){
         $errores[] = "La propiedad debe de tener un vendedor";
     }
 
+   if(!$imagen['name']){
+       $errores[] = 'Es obligatorio poner una imagen';
+   }
+
+
+    //Tamano de las imagenes 
+    $medida = 1000 * 1000;
+    if ($imagen['size'] > $medida ){
+       $errores[] = 'La imagen es muy pesada';
+    }
+
     //echo "<pre>";
-    //var_dump($errores);
-    //echo "</pre>";
+   // var_dump($errores);
+   // echo "</pre>";
+   
     
 
 
     if(empty($errores)){
+
+        $carpetaImagenes = '../../imagenes/';
+
+        if(!is_dir($carpetaImagenes)){
+
+            mkdir($carpetaImagenes);
+        }
+
+
+        // Renombramiento de imagenes 
+
+        $nombreImagen = md5(uniqid(rand(),true)) . ".jpg";
+      
+       
+        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes  . $nombreImagen );
+      exit;
+
+
+
  // insertar en la base de datos 
  $query =" INSERT INTO propiedades (titulo, precio, descripcion, 
  luz, agua, vista, vendedorId)  VALUES('$titulo', '$precio', '$descripcion', '$luz', '$agua','$vista','$vendedorId')";
@@ -61,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] ==='POST'){
 
  $resultado = mysqli_query($db, $query);
  if($resultado){
-     echo "Insertado Correctamente";
+    header('location: /admin');
  }
 
     }
@@ -96,24 +148,24 @@ incluirTemplate('header');
 
 
 
-    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php" enctype="multipart/form-data">
         <fieldset>
             <legend>Informacion general</legend>
 
             <label for="titulo">Titulo:</label>
-            <input type="text" id="titulo" name="titulo" placeholder="Titulo de la propiedad">
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo de la propiedad" value="<?php echo $titulo; ?>">
 
             <br>
             <label for="precio">Precio:</label>
-            <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
+            <input type="number" id="precio" name="precio" value="<?php echo $precio; ?>">
 
             <br>
             <label for="imagen">Imagen:</label>
-            <input type="file" id="imagen" accept="image.jpeg, image/png" >
+            <input type="file" id="imagen" accept="image.jpeg, image/png" name="imagen" >
             <br>
-            <label for="descripcion">Descripcion</label>
+            <label for="descripcion">Descripcion</label >
             <br>
-            <textarea id="descripcion"  name="descripcion"placeholder="Escriba una descripcion de la propiedad "cols="60" rows="10"></textarea>
+            <textarea id="descripcion"  name="descripcion"placeholder="Escriba una descripcion de la propiedad "cols="60" rows="10" ><?php echo $descripcion; ?></textarea>
         </fieldset>
 
 
@@ -122,19 +174,19 @@ incluirTemplate('header');
         <legend>Informacion de la propiedad</legend>
 
         <label for="luz" >electricidad:</label>
-        <input type="text" id="luz" name="luz" >
+        <input type="text" id="luz" name="luz" value="<?php echo $luz; ?>">
 <br>
-            <label for="agua">Agua:</label>
-            <input type="text" id="agua" name="agua" >
+            <label for="agua">Agua:</label >
+            <input type="text" id="agua" name="agua"  value="<?php echo $agua; ?>">
 <br>
-            <label for="vista">Panorama:</label>
-            <input type="text" id="vista" name="vista" >
+            <label for="vista">Panorama:</label >
+            <input type="text" id="vista" name="vista" value="<?php echo $vista; ?>" >
 
 
         </fieldset>
 
         <fieldset>
-            <legend>Vendedor</legend>
+            <legend>Vendedor</legend >
             <select  name="vendedor">
                 <option id="vendedor" value="1" >CemProdeca</option>
                 <option id="vendedor" value="2" >IMAS</option>
