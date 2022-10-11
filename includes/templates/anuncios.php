b3b2bd7c9f750f278e71ae9dbd56d27e95da4dbc
JorgeVillaCar <jorge.villalobos.carranza@est.una.ac.cr>
2022-10-11 14:54:49 -0600

    Llama las propiedades de BD al index principal

---
+++ includes/templates/anuncios.php
@@ -0,0 +1,62 @@
+<?php
+
+//importar la conexion
+require __DIR__ .'/../config/database.php';
+$db =   conectarDB();
+
+// consultar
+$query = "SELECT * FROM propiedades LIMIT ${limite}";
+
+//obtener resultados
+$resultado = mysqli_query($db, $query);
+
+?>
+
+
+<div class="contenedor-anuncios">
+    <?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
+            <div class="anuncio">
+
+
+            <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="anuncio">
+            </picture>
+            
+            <div class="contenido-anuncio">
+                <h3><?php echo $propiedad['nom_proyecto']; ?></h3>
+                <p><?php echo $propiedad['descripcion']; ?></p>
+                <p class="precio"><?php echo $propiedad['precio']; ?></p>
+
+
+                <!-- <ul class="iconos-cararteristicas">
+                    <li>
+                        <img loading="lazy" src="build/img/IconoAgua.svg" alt="">
+                        <p>   No</p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/Electricidad.svg" alt="">
+                        <p>No</p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/Paisaje.svg" alt="">
+                        <p>SI</p>
+                    </li>
+                </ul> -->
+
+                <a href="propiedad.php?id=<?php echo $propiedad['cod_propiedad']; ?>" class="boton-celeste">
+                    Ver Propiedad </a>
+
+                </div><!--.contenido anuncio-->
+            </div><!--anuncio-->
+            <?php endwhile; ?>
+            </div><!--.contenedor anuncios-->
+
+
+            <?php
+
+        //cerrar la conexion
+
+        mysqli_close($db);
+
+
+
+            ?>
--- index.php
+++ index.php
@@ -58,113 +58,20 @@ require 'includes/funciones.php';
     </main>
     <section class="seccion contenedor">
         <h2> Propiedades a la venta </h2>
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
 
-            <picture>
-                <source srcset="build/img/propiedad1.webp" type="image/webp"> 
-                <source srcset="build/img/propiedad1.jpg" type="image/jpg"> 
-                <img loading="lazy" src="build/img/Propiedad1.jpg" alt="anincio">
-            </picture>
+
+        <?php
+
+        $limite = 3;
+        include 'includes/templates/anuncios.php';
+        ?>
             
-            <div class="contenido-anuncio">
-                <h3>Montaña</h3>
-                <p> Montaña con bastante vegetacion</p>
-                <p class="precio">3 000 000</p>
-                <ul class="iconos-cararteristicas">
-                    <li>
-                        <img loading="lazy" src="build/img/IconoAgua.svg" alt="">
-                        <p>   No</p>
-                    </li>
-                    <li>
-                        <img loading="lazy" src="build/img/Electricidad.svg" alt="">
-                        <p>No</p>
-                    </li>
-                    <li>
-                        <img loading="lazy" src="build/img/Paisaje.svg" alt="">
-                        <p>SI</p>
-                    </li>
-                </ul>
-
-                <a href="propiedad.php" class="boton-celeste">
-                    Ver Propiedad
-                </a>
-            </div>
-            </div>
-            <div class="anuncio">
-
-                <picture>
-                    <source srcset="build/img/Propiedad4.webp" type="image/webp"> 
-                    <source srcset="build/img/Propiedad4.jpg" type="image/jpg"> 
-                    <img loading="lazy" src="build/img/Propiedad4.jpg" alt="anincio">
-                </picture>
-                
-                <div class="contenido-anuncio">
-                    <h3>Montaña</h3>
-                    <p> Montaña con bastante vegetacion</p>
-                    <p class="precio">3 000 000</p>
-                    <ul class="iconos-cararteristicas">
-                        <li>
-                            <img loading="lazy" src="build/img/IconoAgua.svg" alt="">
-                            <p>   No</p>
-                        </li>
-                        <li>
-                            <img loading="lazy" src="build/img/Electricidad.svg" alt="">
-                            <p>No</p>
-                        </li>
-                        <li>
-                            <img loading="lazy" src="build/img/Paisaje.svg" alt="">
-                            <p>SI</p>
-                        </li>
-                    </ul>
-    
-                    <a href="propiedad.php" class="boton-celeste">
-                        Ver Propiedad
-                    </a>
-                </div>
-                </div>
-                <div class="anuncio">
-
-                    <picture>
-                        <source srcset="build/img/propiedad 3.webp" type="image/webp"> 
-                        <source srcset="build/img/propiedad 3.jpg" type="image/jpg"> 
-                        <img loading="lazy" src="build/img/Propiedad 3.jpg" alt="anincio">
-                    </picture>
-                    
-                    <div class="contenido-anuncio">
-                        <h3>Montaña</h3>
-                        <p> Montaña con bastante vegetacion</p>
-                        <p class="precio">3 000 000</p>
-                        <ul class="iconos-cararteristicas">
-                            <li>
-                                <img loading="lazy" src="build/img/IconoAgua.svg" alt="">
-                                <p>   No</p>
-                            </li>
-                            <li>
-                                <img loading="lazy" src="build/img/Electricidad.svg" alt="">
-                                <p>No</p>
-                            </li>
-                            <li>
-                                <img loading="lazy" src="build/img/Paisaje.svg" alt="">
-                                <p>SI</p>
-                            </li>
-                        </ul>
-        
-                        <a href="propiedad.php" class="boton-celeste">
-                            Ver Propiedad
-                        </a>
-                    </div>
-                 </div>
-            </div>
-
-            <br>
-            <br>
             <div class="ver-todas">
                 <a class="bton-ver-propiedades" href="propiedades.php"> Ver Todas las Propiedades</a>
             </div>
-        <!--Proyectos-->
-
     </section>
+
+     <!--Proyectos-->
     <section class="seccion contenedor">
         <h2> Proyectos financiados por cemprodeca </h2>
         <div class="contenedor-anuncios">
