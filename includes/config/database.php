8767d647635438d9ceff7a802ad9194e094c07fd
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-11-10 21:32:55 -0600

    Variables de entorno

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,19 @@
 <?php
 function conectarDB() : mysqli {
 
-    $db = mysqli_connect('localhost','root','root','cemprodeca');
+    $db = mysqli_connect(
+        $_ENV['DB_HOST'] ,
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_BD']
+    );
    // $db = mysqli_connect('localhost','root','root','pruebas_crudImagenPropiedadProyecto2');
+
 
     if(!$db){
         echo "Error no se pudo conectar";
         exit;
-    }
+    }else
 
     return $db;
 }
