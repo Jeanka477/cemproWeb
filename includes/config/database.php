97815282ae075b998cc1b7cf080b01cdb8a67889
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-11-11 14:48:17 -0600

    conexion heroku

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,10 +1,9 @@
 <?php
 function conectarDB() : mysqli {
 
-
+    //mysql://bbd6310f71c718:b3f72c30@us-cdbr-east-06.cleardb.net/heroku_be4e81f72c98ffa?reconnect=true
     //$db = mysqli_connect('us-cdbr-east-06.cleardb.net','bbd6310f71c718','b3f72c30','heroku_be4e81f72c98ffa');
     $db = mysqli_connect('localhost','root','root','cemprodeca');
-
 
     if(!$db){
         echo "Error no se pudo conectar";
