7fcae29fd3877ba4c5c83859a54dcc3fca6809d7
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-11-10 23:30:36 -0600

    conexion bd

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,13 @@
 <?php
 function conectarDB() : mysqli {
 
-    $db = mysqli_connect(
-       $_ENV['DB_HOST'] ,
-       $_ENV['DB_USER'],
-        $_ENV['DB_PASS'],
-        $_ENV['DB_BD']
-    );
-    //$db = mysqli_connect('us-cdbr-east-06.cleardb.net','bbd6310f71c718','b3f72c30','heroku_be4e81f72c98ffa');
+  //  $db = mysqli_connect(
+  //     $_ENV['DB_HOST'] ,
+  //     $_ENV['DB_USER'],
+   //     $_ENV['DB_PASS'],
+  //      $_ENV['DB_BD']
+    //);
+    $db = mysqli_connect('us-cdbr-east-06.cleardb.net','bbd6310f71c718','b3f72c30','heroku_be4e81f72c98ffa');
 
 
     if(!$db){
