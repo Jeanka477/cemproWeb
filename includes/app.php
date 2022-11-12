f31490643aa1eaf88ab4a6e8542774dccb1771ac
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-11-12 12:37:54 -0600

    1

--- includes/app.php
+++ includes/app.php
@@ -1,18 +1,13 @@
-
-//require __DIR__ . '/../vendor/autoload.php';
-
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-//$dotenv->safeLoad();
-//require 'funciones.php';
-//require 'config/database.php';
-
-
-//use App\Propiedad;
-
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 require 'funciones.php';
 require 'config/database.php';
-//require __DIR__ . '/../vendor/autoload.php';
+
 
 //use App\Propiedad;
+
+
+
