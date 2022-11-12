3415c8e7b1bbea80ab7f29dd35418eefb6d90954
Jeanka477 <98825823+Jeanka477@users.noreply.github.com>
2022-11-12 09:40:15 -0600

    carpeta public

--- includes/app.php
+++ includes/app.php
@@ -1,11 +1,18 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
+//require __DIR__ . '/../vendor/autoload.php';
+
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->safeLoad();
+//require 'funciones.php';
+//require 'config/database.php';
+
+
+//use App\Propiedad;
+
+<?php
+
 require 'funciones.php';
 require 'config/database.php';
-
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\Propiedad;
-
--- includes/config/database.php
+++ includes/config/database.php
@@ -3,7 +3,7 @@ function conectarDB() : mysqli {
 
     //mysql://bbd6310f71c718:b3f72c30@us-cdbr-east-06.cleardb.net/heroku_be4e81f72c98ffa?reconnect=true
     //$db = mysqli_connect('us-cdbr-east-06.cleardb.net','bbd6310f71c718','b3f72c30','heroku_be4e81f72c98ffa');
-    $db = mysqli_connect('localhost','root','root','cemprodeca');
+    $db = mysqli_connect('us-cdbr-east-06.cleardb.net','bbd6310f71c718','b3f72c30','heroku_be4e81f72c98ffa');
 
     if(!$db){
         echo "Error no se pudo conectar";
